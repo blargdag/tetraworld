9d674268ef8025a3138fe97c3a8a5e19ab279d42
blargdag <blargdag@quickfur.ath.cx>
2014-06-20 10:18:13 -0700

    Integrate arsd.terminal, arsd.eventloop into main program.

--- SConstruct
+++ SConstruct
@@ -1,25 +1,72 @@
 #!/usr/bin/scons
+import os;
+
+#
+# Command-line configurable parameters
+#
 
 debug = ARGUMENTS.get('debug', 0)
 release = ARGUMENTS.get('release', 0)
 
+
+#
+# Static configuration parameters
+#
+
+dmd = '/usr/src/d/dmd/src/dmd'
+
+arsd_incdir = '../ext'
+arsd_path = arsd_incdir + os.sep + 'arsd'
+arsd_flags = ['-version=with_eventloop']
+
+
+#
+# Environment setup
+#
+
+dflags = []
 if release:
-	dflags = '-release'
+	dflags += ['-release']
 else:
-	dflags = '-unittest'
+	dflags += ['-unittest']
 
 if debug:
-	dflags = dflags + ' -g -debug'
+	dflags += ['-g', '-debug']
 else:
-	dflags = dflags + ' -O'
+	dflags += ['-O']
+
+dflags = dflags + ['-I' + arsd_incdir]
+
+
+#
+# Build rules
+#
 
 env = Environment(
-	DMD = '/usr/src/d/dmd/src/dmd',
-	DFLAGS = dflags
+	DMD = dmd,
+	DFLAGS = dflags,
+
+	DCOM = "$DMD $DFLAGS -of$TARGET $SOURCES",
+
+	ARSD_FLAGS = arsd_flags,
+	ARSD_COM = "$DMD $DFLAGS $ARSD_FLAGS -of$TARGET -c $SOURCE"
 )
 
+# Tetraworld main program
 env.Command('tetraworld', Split("""
 		tetraworld.d
+
+		eventloop.o
+		terminal.o
 	"""),
 	"$DMD $DFLAGS -of$TARGET $SOURCES"
+)
+
+
+# arsd modules
+env.Command('eventloop.o', arsd_path + os.sep + 'eventloop.d',
+	"$ARSD_COM"
+)
+env.Command('terminal.o', arsd_path + os.sep + 'terminal.d',
+	"$ARSD_COM"
 )
--- tetraworld.d
+++ tetraworld.d
@@ -1,6 +1,55 @@
-/+
- + Yet another 4D world.
- +/
+/**
+ * Yet another 4D world.
+ *
+ * Copyright: (C) 2012-2021  blargdag@quickfur.ath.cx
+ *
+ * This file is part of Tetraworld.
+ *
+ * Tetraworld is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * Tetraworld is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Tetraworld.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-void main() {
+import arsd.eventloop;
+import arsd.terminal;
+
+void main()
+{
+    auto term = Terminal(ConsoleOutputType.cellular);
+    auto input = RealTimeConsoleInput(&term, ConsoleInputFlags.raw);
+
+    addListener((InputEvent event) {
+        switch (event.type)
+        {
+            case InputEvent.Type.CharacterEvent:
+                auto ev = event.get!(InputEvent.Type.CharacterEvent);
+                if (ev.eventType == CharacterEvent.Type.Pressed)
+                {
+                    switch (ev.character)
+                    {
+                        case 'q':
+                            arsd.eventloop.exit();
+                        default:
+                            break;
+                    }
+                }
+                break;
+
+            default:
+                break;
+        }
+    });
+
+    loop();
 }
+
+// vim:set ai sw=4 ts=4 et:
