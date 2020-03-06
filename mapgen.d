1a1209d8915756da33fafb5e181a71ea2e86e5ae
blargdag <blargdag@quickfur.ath.cx>
2020-03-06 11:43:36 -0800

    Bring conical creature HP up to max again.
    Now that rock traps are an efficient way to kill them, we have to make
    it harder for the player. :-D

--- mapgen.d
+++ mapgen.d
@@ -1405,7 +1405,7 @@ World genBspLevel(MapGenArgs args, out int[4] startPos)
 
         w.store.createObj(Pos(pos), Name("conical creature"),
                           Tiled(TileId.creatureA, 1, Tiled.Hint.dynamic),
-                          BlocksMovement(), Agent(), Mortal(5,2),
+                          BlocksMovement(), Agent(), Mortal(5,5),
                           CanMove(CanMove.Type.walk | CanMove.Type.climb));
     }
 
