bffb0500cf87d86ba116e19ed0adb294cb98d1a7
blargdag <blargdag@quickfur.ath.cx>
2020-11-04 06:27:06 -0800

    Delete old obsolete code.
    Update TODO.

--- ai.d
+++ ai.d
@@ -320,59 +320,6 @@ struct SysAi
         // and plan a new course of action.
         return false;
     }
-
-    version(none)
-    Action __OLD__chooseAiAction(World w, ThingId agentId)
-    {
-        auto agent = w.store.getObj(agentId);
-        auto curPos = *w.store.get!Pos(agentId);
-
-        // For now, chase player.
-        auto r = w.store.getAll!Agent()
-                  .filter!(id => w.store.get!Agent(id).type == Agent.Type.player);
-        if (!r.empty)
-        {
-            auto targetId = r.front;
-            auto targetPos = *w.store.get!Pos(targetId);
-
-            if (w.canSee(curPos, targetPos))
-            {
-                auto inven = w.store.get!Inventory(agentId);
-                auto contents = inven ? inven.contents : [];
-                auto weapons = contents
-                    .filter!(item => item.inEffect &&
-                                     w.store.get!Weapon(item.id) !is null)
-                    .map!(item => item.id);
-
-                auto diff = targetPos - curPos;
-                if (!weapons.empty && diff[].map!(x => abs(x)).sum == 1)
-                {
-                    // Adjacent to player. Attack!
-                    import rndutil : pickOne;
-                    return (World w) => attack(w, agent, targetId, weapons.pickOne);
-                }
-
-                // If no weapons, flee from player instead of attack.
-                if (weapons.empty)
-                    diff = -diff;
-
-                int[4] dir;
-                if (findViableMove(w, agentId, curPos, 6, () => chooseDir(diff),
-                                   dir))
-                    return (World w) => move(w, agent, vec(dir));
-            }
-            // Couldn't find a way to reach target, or can't see target, fallback
-            // to random move.
-        }
-
-        // Nothing to do, just wander aimlessly.
-        int[4] dir;
-        if (findViableMove(w, agentId, curPos, 6, () => dir2vec(randomDir), dir))
-            return (World w) => move(w, agent, vec(dir));
-
-        // Can't even do that; give up.
-        return (World w) => pass(w, agent);
-    }
 }
 
 // vim:set ai sw=4 ts=4 et:
