1c5d1c7d323ed6301f1ff00d090067f26fdd4be9
blargdag <blargdag@quickfur.ath.cx>
2021-02-11 01:50:31 -0800

    Room volume/floor area should not be saved.

--- gamemap.d
+++ gamemap.d
@@ -735,7 +735,7 @@ class MapNode : Saveable!(MapNode, BspNode!(MapNode))
             _floorArea = left.floorArea() + right.floorArea();
         return _floorArea;
     }
-    private int _floorArea = int.min;
+    @NoSave private int _floorArea = int.min;
 
     /**
      * Returns: The total volume of this part of the map.
@@ -747,7 +747,7 @@ class MapNode : Saveable!(MapNode, BspNode!(MapNode))
             _volume = left.volume + right.volume;
         return _volume;
     }
-    private int _volume = int.min;
+    @NoSave private int _volume = int.min;
 
     /**
      * Look up a location on the map.
